37ad71d99e21b5a1d28adcefbfebbc1cae222943
Cihat Aslan <109680094+cihat-aslan@users.noreply.github.com>
2026-01-14 14:23:48 +0300

    Mobil APK ve download butonu eklendi

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // ANDROID EMULATOR: 10.0.2.2
 // PHYSICAL DEVICE: Your LAN IP (e.g., 192.168.1.X)
-// AWS PRODUCTION: http://<your-ec2-ip>:8080
-const API_URL = 'http://10.164.247.157:8080/api';
+// AWS PRODUCTION: http://56.228.13.31:8080
+const API_URL = 'http://56.228.13.31:8080/api';
 
 const api = axios.create({
     baseURL: API_URL,
